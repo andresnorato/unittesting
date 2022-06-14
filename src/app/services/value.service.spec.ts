88af3c272583beb19b83af8767bb79dd41ba9dde
Andres Norato <carnorat@gmail.com>
2022-06-13 23:19:51 -0500

    First UniTest

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,61 @@
+
+import { ValueService } from './value.service';
+
+fdescribe('ValueService', () => {
+  let service: ValueService;
+
+  beforeEach(() => {
+    service = new ValueService();
+  })
+
+
+  it('should be create', () => {
+    expect(service).toBeTruthy();
+  });
+
+
+  describe('Tests for getValue', () => {
+    it('should return "my value"', () => {
+      expect(service.getValue()).toBe('my value');
+    });
+  });
+
+
+  describe('Tests for setValue', () => {
+    it('should change the value"', () => {
+      expect(service.getValue()).toBe('my value');
+      service.setValue('change');
+      expect(service.getValue()).toBe('change');
+    });
+  });
+
+
+
+  describe('Tests for getPromiseValue', () => {
+    it('should return "promiseValue from promise with then"', (doneFn) => {
+      service.getPromiseValue()
+        .then((value) => {
+          expect(value).toBe('Promise value')
+          doneFn();
+        });
+    });
+  });
+
+
+  it('should return "promiseValue from promise using async', async () => {
+    const rta = await service.getPromiseValue()
+    expect(rta).toBe('Promise value');
+  });
+
+
+  describe('Tests for getObservable', () => {
+    it('should return "Observable value" from observable', (doneFn) => {
+      service.getObservableValue()
+      .subscribe(rta =>{
+        expect(rta).toBe('Observable value');
+        doneFn();
+      })
+    })
+  });
+
+});
